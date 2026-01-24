ab210d2c5324a62d7be124cf256e20b136532516
Giridhar Sreekumar <sreekumargiridhar@gmail.com>
2026-01-25 03:51:54 +0530

    Updated brute force and optimized solution for Leetcode 242(Valid Anagram)

--- strings/Easy/242_ValidAnagram.cpp
+++ strings/Easy/242_ValidAnagram.cpp
@@ -1,4 +1,6 @@
 // Problem: 242. Valid Anagram
+
+
 // Approach: Frequency Counting (Hash Map)
 // Time Complexity: O(n)
 // Space Complexity: O(n)
@@ -26,6 +28,37 @@ public:
                 return false;
             }
         }
+        return true;
+    }
+};
+
+
+// -----------------------------------------------------------
+// Approach: Frequency Counting (Array / Vector)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        vector<int> freq(26, 0);   //Vector used cause all characters are lowercase, therefore, range fixed.
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s[i] - 'a']++;    //single step vector linking, s[i] character frequencies are added
+            freq[t[i] - 'a']--;     //t[i] character frequencies are subtracted
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {         //Check if any of vectors have frequency below zero
+                return false;
+            }
+        }
+
         return true;
     }
 };
