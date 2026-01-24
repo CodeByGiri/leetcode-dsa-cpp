9198daa300e56580514ddd0e21d9d2348d01a556
Giridhar Sreekumar <sreekumargiridhar@gmail.com>
2026-01-25 04:19:56 +0530

    Vector solution included for Leetcode 387(First Unique Character)

---
+++ strings/Easy/387_First_Unique_Character_In_a_String.cpp
@@ -0,0 +1,26 @@
+// Problem: 387. First Unique Character in a String
+// Approach: Frequency Counting (Vector)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+
+        vector<int> freq(26, 0);  //Since all characters lowercase, use vector instead of maps
+
+        for (int i = 0; i < s.length(); i++) {   
+            freq[s[i] - 'a']++;         //Counts frequency of each character in string
+        }
+
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s[i] - 'a'] == 1) {       //If any character occured only one time in string, return index
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
