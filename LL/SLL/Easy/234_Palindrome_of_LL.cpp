9ae3d5a8fc0cac283ecd618b30038fc2ba894d24
Giridhar Sreekumar <sreekumargiridhar@gmail.com>
2026-01-30 20:32:26 +0530

    Solution provided for LeetCode 234(Palindrome of LL)

---
+++ LL/SLL/Easy/234_Palindrome_of_LL.cpp
@@ -0,0 +1,40 @@
+// Problem: 234. Palindrome Linked List
+// Technique: Slow & Fast Pointers + In-place Reversal
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        
+        if(head == NULL) return false;
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while(fast!=NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        //Now slow is the head of the second half of our LL, we'll reverse this half now
+
+        ListNode *prev = NULL;
+        ListNode *current = slow;
+
+        while(current != NULL){
+            ListNode *next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        ListNode *head1 = head;
+        while(prev != NULL){
+            if(head1->val != prev->val) return false;
+            head1 = head1->next;
+            prev=prev->next;
+        }
+        return true;
+    }
+};
