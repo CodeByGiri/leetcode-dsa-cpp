bb40d2a75ea662f9288b4e9196841a9ca53169da
Giridhar Sreekumar <sreekumargiridhar@gmail.com>
2026-01-30 18:10:07 +0530

    Added solution for Leetcode 876 (Middle Node of LL)

---
+++ LL/SLL/Easy/876_Middle_of_LL.cpp
@@ -0,0 +1,27 @@
+// Problem: 876. Middle of the Linked List
+// Technique: Counting Nodes
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head){
+    int count = 0;
+    ListNode* temp = head;
+
+    while(temp != NULL){  
+        count++;    //Get total count of nodes in LL
+        temp = temp->next;
+    }
+
+    int mid = count / 2;
+    temp = head;
+
+    for(int i = 0; i < mid; i++){
+        temp = temp->next;   //Find the middle node
+    }
+
+    return temp;
+
+    }
+};
