a97e9f4452e71faf4cb9a714d8aa05c0648f04e5
Giridhar Sreekumar <sreekumargiridhar@gmail.com>
2026-02-15 17:23:57 +0530

    Writeup with comment can be provided for LeetCode 232(Queue using Stacks)

--- Stacks/Easy/232_Implement_Queue_using_Stacks.cpp
+++ Stacks/Easy/232_Implement_Queue_using_Stacks.cpp
@@ -1,3 +1,12 @@
+// Problem: 232. Implement Queue using Stacks
+// Technique: Two Stacks (inStack + outStack)
+// Time Complexity: Amortized O(1)
+// Space Complexity: O(n)
+// Idea:
+// Push elements into inStack.
+// For pop/peek, if outStack is empty, pour all elements from inStack into outStack.
+// This reverses order and gives FIFO behavior.
+
 class MyQueue {
 public:
 
