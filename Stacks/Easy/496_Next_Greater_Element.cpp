312ef74829c84d37ade72ba80c6540ec96b4a8e9
Giridhar Sreekumar <sreekumargiridhar@gmail.com>
2026-02-15 18:23:52 +0530

    Updated writeup provided for LeetCode 496(Next Greater Element

--- Stacks/Easy/496_Next_Greater_Element.cpp
+++ Stacks/Easy/496_Next_Greater_Element.cpp
@@ -1,3 +1,13 @@
+// Problem: 496. Next Greater Element I
+// Technique: Next Greater Element Mapping
+// Time Complexity: O(n + m)
+// Space Complexity: O(n)
+
+//Idea : We traverse through nums2 array with a monotic decreasing stack.
+// We push each element one by one into the stack and compare that element with the upcomming elements to see which is the next greater element to it in the array.
+// If element exists. we map the element in the stack to the greater element found.
+//After the traversal, if elements are there with no greater elements, they are gonna be present in the stack. We map these elements to -1 and pop them from stack until stack is empty.
+// Now, traverse nums1 and push all the respective values for the elements from the map into the ans array and return it.
 
 class Solution {
 public:
