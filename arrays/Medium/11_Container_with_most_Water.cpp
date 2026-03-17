e7bbf5fec36f3fed26490779da1d1c13b260ee7b
Giridhar Sreekumar <sreekumargiridhar@gmail.com>
2026-03-18 01:29:09 +0530

    Code for Leetcode 167(Two Sum II)

---
+++ arrays/Medium/11_Container_with_most_Water.cpp
@@ -0,0 +1,31 @@
+// Problem: 11. Container With Most Water
+// Technique: Two Pointers (Greedy)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+//area = width * height 
+// width is difference between last and first pointer
+//height is the shorter height of the height of lines we choose
+
+//objective is to maximize area, so we need to take two lines in such a way such that min(height) is the a large value and a decent amt of width is possible
+//ex: a scenario where we have width 5 and height of lines as(6,8) is much better than a width of 6 and height of lines of(3,8) cause area is more for the former.
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0, r = height.size() - 1;
+        int maxArea = 0;
+
+        while(l < r){
+            int h = min(height[l], height[r]);
+            int width = r-l;
+            int area = width * h;
+
+            maxArea = max(maxArea, area);
+
+            if(height[l] < height[r]) l++;
+            else r--;
+        }
+        return maxArea;
+    }
+};
---
+++ arrays/Medium/35_Search_in_Rotated_Array.cpp
@@ -0,0 +1,48 @@
+// Problem: 33. Search in Rotated Sorted Array
+// Technique: Modified Binary Search
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+///
+///
+// Idea: When an array is rotated, only half of it would be sorted, the other half is gonna have a disruption due to the presence of mid element. So we check which side is sorted first.
+//Once we find the sorted side, we try to check the presence of target in that side since that half is sorted and it acts like normal binary search. 
+//If no elements are found, we return -1.
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+
+        int low = 0;
+        int high = n-1;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) return mid;
+            
+            //Check which half sorted
+            if(nums[low] <= nums[mid]){
+
+                //When left half sorted, see if target inside left half
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{  //right half sorted
+
+                //check if element in right half
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
