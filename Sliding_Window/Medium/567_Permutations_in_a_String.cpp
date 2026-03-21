8dbe88577a5a0632f0ceaa99ef53f158d57146c2
Giridhar Sreekumar <sreekumargiridhar@gmail.com>
2026-03-21 17:16:11 +0530

    Code implemented for LC 567(Permutations in a String)

---
+++ Sliding_Window/Medium/567_Permutations_in_a_String.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length()) return false;
+
+        vector<int> count(26,0);
+        for(char c : s1){
+            count[c - 'a']++;
+        }
+
+        int k = s1.length(); //window size
+        vector<int> target(26,0);
+
+        for(int i = 0; i < k ; i++){
+            target[s2[i] - 'a']++;
+        }
+        if(count == target) return true;
+
+        int l = 0;
+        for(int r = k; r < s2.length(); r++){
+            target[s2[r] - 'a']++;
+            target[s2[l] - 'a']--;
+            
+            l++;
+            if(count == target) return true;
+        }
+
+        return false;
+    }
+};
