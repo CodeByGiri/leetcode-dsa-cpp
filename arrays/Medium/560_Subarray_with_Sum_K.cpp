45f027eb1281e2f38519286fa8d62c3964f48cb8
Giridhar Sreekumar <sreekumargiridhar@gmail.com>
2026-03-24 23:14:34 +0530

    Commit for LC 560(Subarray with Sum K

--- arrays/Medium/560_Subarray_with_Sum_K.cpp
+++ arrays/Medium/560_Subarray_with_Sum_K.cpp
@@ -1,3 +1,6 @@
+//560.Subarray with Sum K
+//Method: Hashing
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
@@ -5,12 +8,12 @@ public:
         unordered_map<int,int> mp;
 
         int sum = 0, count = 0;
-        mp[0] = 1;
+        mp[0] = 1;   //take array from index 0
         
         for(int num : nums){
             sum += num;
 
-            if(mp.find(sum-k) != mp.end()){
+            if(mp.find(sum-k) != mp.end()){ 
                 count += mp[sum-k];
             }
 
