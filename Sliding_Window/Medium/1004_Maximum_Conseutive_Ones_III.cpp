b01a1140af58754b83f7b6e5bc6f6250e433b2d2
Giridhar Sreekumar <sreekumargiridhar@gmail.com>
2026-03-24 23:16:45 +0530

    Solution for LC 1004(Maximum Consecutive Ones III)

---
+++ Sliding_Window/Medium/1004_Maximum_Conseutive_Ones_III.cpp
@@ -0,0 +1,24 @@
+//1004. Max Consecutive Ones III
+//Method: Sliding Window
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        
+        int count = 0, l = 0, maxLen = 0;
+
+        for(int r = 0; r<nums.size(); r++){
+            if(nums[r] == 0) count++;
+
+            while(count > k){
+                if(nums[l] == 0) count--;
+                l++;
+            }
+            maxLen = max(maxLen, (r-l+1));
+        }
+
+        return maxLen;
+    }
+};
