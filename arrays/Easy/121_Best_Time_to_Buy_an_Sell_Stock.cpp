b1e5457027c78ca51c7a2e22767fd9862a53b943
Giridhar Sreekumar <sreekumargiridhar@gmail.com>
2026-03-30 01:26:13 +0530

    Writeup for LC 121

--- arrays/Easy/121_Best_Time_to_Buy_an_Sell_Stock.cpp
+++ arrays/Easy/121_Best_Time_to_Buy_an_Sell_Stock.cpp
@@ -1,4 +1,6 @@
-
+//121. Best Time to Buy an Sell Stock
+// Time Complexity : O(n)
+// One of the easiest problems. go through the vector, find the minPrice each step, and at the same time, reduce it from the price to see if it yields maxProfit
 
 class Solution {
 public:
