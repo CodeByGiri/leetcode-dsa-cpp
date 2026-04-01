cd2e51ad00febedc349f33fd4a6e43ef7c72927b
Giridhar <sreekumargiridhar@gmail.com>
2026-04-02 01:02:08 +0530

    Update 169_Majority_Element.cpp

--- arrays/Easy/169_Majority_Element.cpp
+++ arrays/Easy/169_Majority_Element.cpp
@@ -1,5 +1,8 @@
 //169.Majority Element
 //Array Based
+//We are trying to find the mode of an array, i.e, the element which appears the most no of times in the array.
+//We start by considering an element to be the candidate for the mode element, and as soon as we find it repeating, we count the no of occurences in another variable count 
+//And as soon as we encounter a new element, we consider it to be the candidate and repeat the process.
 
 class Solution {
 public:
