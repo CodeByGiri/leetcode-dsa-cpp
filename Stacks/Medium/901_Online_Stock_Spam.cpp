d206cd959fd01e9b027c8e38b4116177a830e2aa
Giridhar <sreekumargiridhar@gmail.com>
2026-04-17 02:22:54 +0530

    Writeup provided for LC 901(Online Stock Spam)

--- Stacks/Medium/901_Online_Stock_Spam.cpp
+++ Stacks/Medium/901_Online_Stock_Spam.cpp
@@ -1,4 +1,7 @@
-
+//901. Online Stock Spam
+//Topic: Stacks
+// In this, stack holds a pair of both price and span for which it stayed highest in an array and then that is pushed into the stack.
+//Basically, next greater but here it's a pair
 class StockSpanner {
 public:
 
